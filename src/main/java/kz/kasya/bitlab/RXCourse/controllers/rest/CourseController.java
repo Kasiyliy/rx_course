8f1c2b323979b5fcd654afd668e0f273876d6256
air <asilkhan-al@mail.ru>
2019-08-09 05:04:13 +0600

    Dtos names fixed

--- src/main/java/kz/kasya/bitlab/RXCourse/controllers/rest/CourseController.java
+++ src/main/java/kz/kasya/bitlab/RXCourse/controllers/rest/CourseController.java
@@ -8,10 +8,13 @@ import kz.kasya.bitlab.RXCourse.models.dtos.CourseDto;
 import kz.kasya.bitlab.RXCourse.models.entities.Course;
 import kz.kasya.bitlab.RXCourse.models.mappers.CourseMapper;
 import kz.kasya.bitlab.RXCourse.services.CourseService;
+import kz.kasya.bitlab.RXCourse.services.UserService;
 import kz.kasya.bitlab.RXCourse.shared.utils.responses.SuccessResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,9 +24,13 @@ public class CourseController extends BaseController {
 
     private CourseService courseService;
     private CourseMapper courseMapper;
+    private UserService userService;
 
     @Autowired
-    public CourseController(CourseService courseService, CourseMapper courseMapper) {
+    public CourseController(UserService userService,
+                            CourseService courseService,
+                            CourseMapper courseMapper) {
+        this.userService = userService;
         this.courseService = courseService;
         this.courseMapper = courseMapper;
     }
