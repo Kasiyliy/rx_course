5953abd49f03cc9cf8480d714a14e3271852627d
air <asilkhan-al@mail.ru>
2019-12-01 06:24:14 +0600

    PreAuthorize added to category controller

--- src/main/java/kz/kasya/bitlab/RXCourse/models/dtos/UserDto.java
+++ src/main/java/kz/kasya/bitlab/RXCourse/models/dtos/UserDto.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 
 @Data
 @NoArgsConstructor
@@ -13,18 +16,28 @@ import lombok.NoArgsConstructor;
 public class UserDto extends BaseDto {
 
     @ApiModelProperty(notes = "Имя", readOnly = true)
+    @NotNull
+    @NotEmpty
     private String firstName;
 
     @ApiModelProperty(notes = "Фамилия", readOnly = true)
+    @NotNull
+    @NotEmpty
     private String lastName;
 
     @ApiModelProperty(notes = "Мобильный номер", readOnly = true)
+    @NotNull
+    @NotEmpty
     private String phoneNumber;
 
     @ApiModelProperty(notes = "Логин", readOnly = true)
+    @NotNull
+    @NotEmpty
     private String login;
 
     @ApiModelProperty(notes = "Пароль", readOnly = true)
+    @NotNull
+    @NotEmpty
     private String password;
 
     @ApiModelProperty(notes = "Роль", readOnly = true)
